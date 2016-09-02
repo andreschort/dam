57f0222169f18a9f114c3aeeb434e296c35f6f01
Andres Chort <andres.chort@gmail.com>
2016-09-01 21:31:26 -0300

    Lab02 Spinner con horarios

--- Lab02/app/src/main/java/ar/edu/utn/frsf/isi/dam/lab02/MainActivity.java
+++ Lab02/app/src/main/java/ar/edu/utn/frsf/isi/dam/lab02/MainActivity.java
@@ -2,6 +2,8 @@ package ar.edu.utn.frsf.isi.dam.lab02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spHorario = (Spinner) this.findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.sp_horario, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spHorario.setAdapter(adapter);
     }
 }
