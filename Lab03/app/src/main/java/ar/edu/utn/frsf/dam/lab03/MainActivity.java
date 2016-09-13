3bfcc8a82a05e7a125bd74b88d9befab010301dc
Andres Chort <andres.chort@gmail.com>
2016-09-13 00:42:22 -0300

    Lab03 CrearOfertaActivity

--- Lab03/app/src/main/java/ar/edu/utn/frsf/dam/lab03/MainActivity.java
+++ Lab03/app/src/main/java/ar/edu/utn/frsf/dam/lab03/MainActivity.java
@@ -1,9 +1,14 @@
 package ar.edu.utn.frsf.dam.lab03;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,5 +28,22 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
 
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.MnuOpc1:
+                Intent intent = new Intent(this, CrearOfertaActivity.class);
+                startActivityForResult(intent, 1);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
